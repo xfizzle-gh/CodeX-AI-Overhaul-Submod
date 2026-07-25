8f2c3cc60e1a215c2584bc86bbc323e69199d419
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-25 17:24:17 -0500

    Correct SG parity file content

--- resource/script/multiplayer/units/nato/codex2022.nato.lua
+++ resource/script/multiplayer/units/nato/codex2022.nato.lua
@@ -1,1 +1,8 @@
-/bin/bash: -c: line 1: unexpected EOF while looking for matching `''
+Purchases["codex2022.nato"] = {
+	{Repeat = 0,
+		Units = {
+			{priority = 2.0, type = {"Infantry", "Squad", "Aux", "Class1"}, unit = "codex_bz_rifle_squad(nato)"},
+			{priority = 1.0, type = {"Infantry", "Squad", "Aux", "Class1"}, unit = "codex_bz_fireteam(nato)"},
+		}
+	}
+}
