cef97fdff7017de0f4a938eec94ae5ad1e8f46ec
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-26 10:33:47 -0500

    Classify NATO specialist units as detachments

--- resource/script/multiplayer/units/nato/2022s.nato.lua
+++ resource/script/multiplayer/units/nato/2022s.nato.lua
@@ -4,11 +4,12 @@ local skirmish = {
 	{
 		Repeat = 0,
 		Units = {
-			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_usmc_weapon_at(nato)"},
-			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_usmc_mg(nato)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_usmc_eng(nato)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_inf2_rifle_m3(nato)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "squad_usmc_weapon_at(nato)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "squad_usmc_mg(nato)"},
 			{priority = 0.75, type = {"Infantry", "Team", "Class1"}, unit = "arf_medic(nato)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "squad_usmc_weapon_at_javelin(nato)"},
 			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_skirmish_nato(nato)"},
 		}
 	}
