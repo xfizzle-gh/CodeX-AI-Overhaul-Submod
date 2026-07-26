80cde4b19d2b37a7bc03cb435ca5a26aee597b13
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-26 10:15:08 -0500

    Add PRC DZJ-08 rifle squad to diagnostic buy list

--- resource/script/multiplayer/units/prc/2022s.prc.lua
+++ resource/script/multiplayer/units/prc/2022s.prc.lua
@@ -7,6 +7,7 @@ local skirmish = {
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_rifle(prc)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_mg(prc)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_pf98(prc)"},
+			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_rifle_dzj08(prc)"},
 			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_skirmish_prc(prc)"},
 		}
 	}
