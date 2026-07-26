afa9c6c65ebcde2b7a5e0d6482c5a736b12e9eb8
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-26 10:14:24 -0500

    Add NATO core rifle squad to diagnostic buy list

--- resource/script/multiplayer/units/nato/2022s.nato.lua
+++ resource/script/multiplayer/units/nato/2022s.nato.lua
@@ -7,6 +7,7 @@ local skirmish = {
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_usmc_weapon_at(nato)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_usmc_mg(nato)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_usmc_eng(nato)"},
+			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_inf2_rifle_m3(nato)"},
 			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_skirmish_nato(nato)"},
 		}
 	}
