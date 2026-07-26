f3d297d971e1b6e19d68fa2a427fa19e934dc340
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-26 16:04:32 -0500

    Register both Russia doctrines

--- resource/script/multiplayer/units/rusa/2022s.rusa.lua
+++ resource/script/multiplayer/units/rusa/2022s.rusa.lua
@@ -11,7 +11,8 @@ local skirmish = {
 			{priority = 0.40, type = {"Infantry", "Team", "Medic", "Support", "Class1"}, unit = "rus_22_5(rusa)"},
 			{priority = 0.45, type = {"Infantry", "Team", "Sniper", "Recon", "Support", "Class1"}, unit = "rus90_inf_sniper(rusa)"},
 			{priority = 0.75, type = {"Infantry", "Squad", "Class1"}, unit = "lud_22_1(rusa)"},
-			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_skirmish_rusa(rusa)"},
+			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine(rusa)"},
+			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_dsh(rusa)"},
 		}
 	}
 }
