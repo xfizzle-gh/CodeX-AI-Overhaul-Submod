5e076d74bf961da7e5e659a4b9f68ae047e3ce83
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-26 10:34:03 -0500

    Classify Ukrainian specialist units as detachments

--- resource/script/multiplayer/units/ukr/2022s.ukr.lua
+++ resource/script/multiplayer/units/ukr/2022s.ukr.lua
@@ -6,9 +6,10 @@ local skirmish = {
 		Units = {
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "ter_22_1(ukr)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "93th_alcatraz_rifle(ukr)"},
-			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "93th_alcatraz_mg_pkm(ukr)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "47th_inf_rifle(ukr)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "93th_alcatraz_mg_pkm(ukr)"},
 			{priority = 0.75, type = {"Infantry", "Team", "Class1"}, unit = "ukr_22_5(ukr)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "47th_inf_at(ukr)"},
 			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_skirmish_ukr(ukr)"},
 		}
 	}
