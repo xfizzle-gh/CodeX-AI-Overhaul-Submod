95797d2e220abff1d9f559ec514cd271513c1482
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-26 14:18:29 -0500

    Rebase Batch 3 and add cost-matched unit portraits

--- resource/script/multiplayer/units/ukr/2022s.ukr.lua
+++ resource/script/multiplayer/units/ukr/2022s.ukr.lua
@@ -9,6 +9,7 @@ local skirmish = {
 			{priority = 1.0, type = {"Infantry", "Team", "Class1"}, unit = "93th_alcatraz_mg_pkm(ukr)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "47th_inf_rifle(ukr)"},
 			{priority = 0.5, type = {"Infantry", "Team", "Class1"}, unit = "ukr_22_5(ukr)"},
+			{priority = 0.5, type = {"Infantry", "Team", "Class1"}, unit = "47th_inf_at(ukr)"},
 			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_skirmish_ukr(ukr)"},
 		}
 	}
