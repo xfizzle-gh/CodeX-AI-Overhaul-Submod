1969c02f5ff3ac16f1af5563da43790489834d66
Paul Fiesel <87280532+paulfiesel@users.noreply.github.com>
2026-07-26 10:34:39 -0500

    Classify PRC specialist units as detachments

--- resource/script/multiplayer/units/prc/2022s.prc.lua
+++ resource/script/multiplayer/units/prc/2022s.prc.lua
@@ -5,10 +5,11 @@ local skirmish = {
 		Repeat = 0,
 		Units = {
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_rifle(prc)"},
-			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_mg(prc)"},
-			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_pf98(prc)"},
 			{priority = 1.0, type = {"Infantry", "Squad", "Class1"}, unit = "squad_pla112_rifle_dzj08(prc)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "squad_pla112_mg(prc)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "squad_pla112_pf98(prc)"},
 			{priority = 0.75, type = {"Infantry", "Team", "Class1"}, unit = "squad_pla112_recon(prc)"},
+			{priority = 0.8, type = {"Infantry", "Team", "Class1"}, unit = "squad_pla112_sniper(prc)"},
 			{priority = 0.01, type = {"Doctrine", "Squad", "Class1"}, unit = "doctrine_squad_skirmish_prc(prc)"},
 		}
 	}
